669873f5fbf8f7a756b1e9a544dba01eb70ab3dc
clorange <lorange.christian@gmail.com>
2019-02-22 11:24:05 -0700

    Initial seeding

---
+++ js/main.js
@@ -0,0 +1,127 @@
+  <script type="text/javascript">
+
+
+    $(function(){
+      
+var $filterCheckboxes = $('input[type="checkbox"]');
+
+$filterCheckboxes.on('change', function() {
+
+  var selectedFilters = {};
+
+  $filterCheckboxes.filter(':checked').each(function() {
+
+    if (!selectedFilters.hasOwnProperty(this.name)) {
+      selectedFilters[this.name] = [];
+    }
+
+    selectedFilters[this.name].push(this.value);
+
+  });
+
+  // create a collection containing all of the filterable elements
+  var $filteredResults = $('.venue');
+
+  // loop over the selected filter name -> (array) values pairs
+  $.each(selectedFilters, function(name, filterValues) {
+
+    // filter each .venue element
+    $filteredResults = $filteredResults.filter(function() {
+
+      var matched = false,
+        currentFilterValues = $(this).data('category').split(' ');
+
+      // loop over each category value in the current .venue's data-category
+      $.each(currentFilterValues, function(_, currentFilterValue) {
+
+        // if the current category exists in the selected filters array
+        // set matched to true, and stop looping. as we're ORing in each
+        // set of filters, we only need to match once
+
+        if ($.inArray(currentFilterValue, filterValues) != -1) {
+          matched = true;
+          return false;
+        }
+      });
+
+      // if matched is true the current .venue element is returned
+      return matched;
+
+    });
+  });
+
+  $('.venue').hide().filter($filteredResults).show();
+
+});
+
+
+    });
+
+</script>
+
+
+<script type="text/javascript">
+  
+  function days() {
+   var d = new Date();
+   var n = d.getDay();
+
+   var weekday=new Array(7);
+       weekday[0]="sunday";
+       weekday[1]="monday";
+       weekday[2]="tuesday";
+       weekday[3]="wednesday";
+       weekday[4]="thursday";
+       weekday[5]="friday";
+       weekday[6]="saturday";
+       
+
+
+   var elements = document.getElementsByClassName(weekday[d.getDay()]);
+    for(i=0; i<elements.length; i++) {
+      elements[i].style.display =    'inline';
+    }
+}
+</script>
+
+
+<script>
+// Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  }
+</script>
