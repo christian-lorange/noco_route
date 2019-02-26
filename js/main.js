0da2af79be8eda0f0edc75db4c863611ddb3d48d
DESKTOP-R7E62OP\Christian L'Orange <lorange.christian@gmail.com>
2019-02-25 18:44:16 -0700

    Updated content

--- js/main.js
+++ js/main.js
@@ -1,91 +1,31 @@
-  <script type="text/javascript">
 
+function ad_show(){
+var ad_increment = localStorage.getItem("ad_increment");  //create a tracking variable
 
-    $(function(){
-      
-var $filterCheckboxes = $('input[type="checkbox"]');
-
-$filterCheckboxes.on('change', function() {
-
-  var selectedFilters = {};
-
-  $filterCheckboxes.filter(':checked').each(function() {
-
-    if (!selectedFilters.hasOwnProperty(this.name)) {
-      selectedFilters[this.name] = [];
-    }
-
-    selectedFilters[this.name].push(this.value);
-
-  });
-
-  // create a collection containing all of the filterable elements
-  var $filteredResults = $('.venue');
-
-  // loop over the selected filter name -> (array) values pairs
-  $.each(selectedFilters, function(name, filterValues) {
-
-    // filter each .venue element
-    $filteredResults = $filteredResults.filter(function() {
-
-      var matched = false,
-        currentFilterValues = $(this).data('category').split(' ');
-
-      // loop over each category value in the current .venue's data-category
-      $.each(currentFilterValues, function(_, currentFilterValue) {
-
-        // if the current category exists in the selected filters array
-        // set matched to true, and stop looping. as we're ORing in each
-        // set of filters, we only need to match once
-
-        if ($.inArray(currentFilterValue, filterValues) != -1) {
-          matched = true;
-          return false;
-        }
-      });
-
-      // if matched is true the current .venue element is returned
-      return matched;
-
-    });
-  });
-
-  $('.venue').hide().filter($filteredResults).show();
-
-});
-
-
-    });
-
-</script>
-
-
-<script type="text/javascript">
-  
-  function days() {
-   var d = new Date();
-   var n = d.getDay();
-
-   var weekday=new Array(7);
-       weekday[0]="sunday";
-       weekday[1]="monday";
-       weekday[2]="tuesday";
-       weekday[3]="wednesday";
-       weekday[4]="thursday";
-       weekday[5]="friday";
-       weekday[6]="saturday";
-       
-
-
-   var elements = document.getElementsByClassName(weekday[d.getDay()]);
-    for(i=0; i<elements.length; i++) {
-      elements[i].style.display =    'inline';
-    }
+if (ad_increment == null){                                //if that tracking varialble dosen't exist create one and set to zero
+    localStorage.setItem("ad_increment","0")
 }
-</script>
+
+  var ad_increment_temp = (ad_increment)+0;               //add a digit to the tracking variable on every refresh
+
+  localStorage.setItem("ad_increment",ad_increment_temp)  //update that tracking variable in local storage
+
+if (ad_increment.length >=3){                             //if tracking variable is more than 5 digits reset to 1 digital and show interstitial ad
+  localStorage.setItem("ad_increment","0")
+
+  setTimeout(function (){
+
+  // Something you want delayed.
+    admob.interstitial.show()  
+
+}, 2000); // How long do you want the delay to be (in milliseconds)? 
+
+}
+
+}
 
 
-<script>
+
 // Open the Modal
 function openModal() {
   document.getElementById('myModal').style.display = "block";
@@ -122,6 +62,7 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  
-  }
-</script>
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
